360e96bdf7b4af55c4cde7106b051309cbdda75e
hagt <henning.agt-rickauer@hpi.de>
2015-08-12 14:15:21 +0200

    Dramaqueen source now can read compressed files

--- de.dwerft.ln/src/test/java/de/dwerft/lpdcimport/dramaqueen/DramaQueenConverterTest.java
+++ de.dwerft.ln/src/test/java/de/dwerft/lpdcimport/dramaqueen/DramaQueenConverterTest.java
@@ -1,37 +1,29 @@
 package de.dwerft.lpdcimport.dramaqueen;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import org.junit.Test;
 
 import de.dwerft.lpdc.general.OntologyConstants;
 import de.dwerft.lpdc.importer.dramaqueen.DramaQueenConverter;
 import de.dwerft.lpdc.importer.general.RdfGenerator;
+import de.dwerft.lpdc.sources.DramaQueenSource;
 
 public class DramaQueenConverterTest {
 	
-	private static final String dqFile = "examples/Hansel_Gretel_de.xml";
+	private static final String dqFile = "examples/Hansel_Gretel_de.dq";
 	
 	@Test
 	public void testDramaQueenConverter() {
 		DramaQueenConverter dqc = new DramaQueenConverter(OntologyConstants.ONTOLOGY_FILE, OntologyConstants.ONTOLOGY_FORMAT);
 		
-		FileInputStream fis = null;
-		try {
-			fis = new FileInputStream(dqFile);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		DramaQueenSource dqSource = new DramaQueenSource();
+		InputStream is = dqSource.get(dqFile);
+		if (is != null) {
+			dqc.generate(is);
+			RdfGenerator.writeRDFToFile(dqc.getGeneratedModel(), "examples/Hansel_Gretel_de.ttl");
+//			dqc.getGeneratedModel().write(System.out, "TTL");
 		}
-		
-		if (fis != null) {
-			dqc.generate(fis);
-		}
-		
-		RdfGenerator.writeRDFToFile(dqc.getGeneratedModel(), "examples/Hansel_Gretel_de.ttl");
-		
-		dqc.getGeneratedModel().write(System.out, "TTL");
-		
 	}
 
 }
