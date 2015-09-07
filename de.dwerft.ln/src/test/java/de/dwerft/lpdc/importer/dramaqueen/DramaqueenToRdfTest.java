78268182497daa82262420b67d9f445788271d23
Henning Agt-Rickauer <henning.agt-rickauer@hpi.de>
2015-09-07 12:57:18 +0200

    Comments removed, write to RDF file

--- de.dwerft.ln/src/test/java/de/dwerft/lpdc/importer/dramaqueen/DramaqueenToRdfTest.java
+++ de.dwerft.ln/src/test/java/de/dwerft/lpdc/importer/dramaqueen/DramaqueenToRdfTest.java
@@ -4,138 +4,19 @@ import java.io.InputStream;
 
 import org.junit.Test;
 
-import com.hp.hpl.jena.rdf.model.Model;
-
 import de.dwerft.lpdc.general.OntologyConstants;
 import de.dwerft.lpdc.sources.DramaQueenSource;
 
 public class DramaqueenToRdfTest {
 	
 	private static final String dqFile = "examples/Hansel_Gretel_de.dq";
-	public static final String DRAMAQUEEN_MAPPINGS_FILE = "src/main/resource/dramaqueen.mappings";
+	private static final String outputFile = "examples/Hansel_Gretel_de_new.ttl";
+	private static final String DRAMAQUEEN_MAPPINGS_FILE = "src/main/resource/dramaqueen.mappings";
 
 
 	@Test
 	public void testConverter() {
 		
-		/*
-		
-		Set<MappingDefinition> mappings = new HashSet<MappingDefinition>();
-		
-	
-		mappings.add(
-				new MappingDefinition("/ScriptDocument", null, null,
-						null, null, OntologyConstants.ONTOLOGY_NAMESPACE+"Project", null, null));
-		mappings.add(
-				new MappingDefinition("/ScriptDocument",
-						"id", null, ContentSource.ATTRIBUTE, "id",
-						OntologyConstants.ONTOLOGY_NAMESPACE+"Project", 
-						OntologyConstants.ONTOLOGY_NAMESPACE+"identifierDramaQueen",
-						TargetPropertyType.DATATYPE_PROPERTY));
-		
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/characters/Character", null, null,
-						null, null, OntologyConstants.ONTOLOGY_NAMESPACE+"Character", null, null));
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/characters/Character", "id", null,
-						ContentSource.ATTRIBUTE, "id",
-						OntologyConstants.ONTOLOGY_NAMESPACE+"Character", 
-						OntologyConstants.ONTOLOGY_NAMESPACE+"identifierDramaQueen",
-						TargetPropertyType.DATATYPE_PROPERTY));
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/characters/Character/Property",
-						"id", "0", ContentSource.ATTRIBUTE, "value",
-						OntologyConstants.ONTOLOGY_NAMESPACE+"Character", 
-						OntologyConstants.ONTOLOGY_NAMESPACE+"name",
-						TargetPropertyType.DATATYPE_PROPERTY));
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/characters/Character/Property",
-						"id", "181", ContentSource.ATTRIBUTE, "value",
-						OntologyConstants.ONTOLOGY_NAMESPACE+"Character", 
-						OntologyConstants.ONTOLOGY_NAMESPACE+"fullName",
-						TargetPropertyType.DATATYPE_PROPERTY));
-		
-		
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/characters/Character",
-						null, null,
-						ContentSource.CONTAINMENT, "ScriptDocument",
-						OntologyConstants.ONTOLOGY_NAMESPACE+"Project", 
-						OntologyConstants.ONTOLOGY_NAMESPACE+"hasCharacter",
-						TargetPropertyType.OBJECT_PROPERTY));
-		
-
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/locations/Location",
-						null, null,
-						null, null, OntologyConstants.ONTOLOGY_NAMESPACE+"Set", null, null));
-
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/locations/Location",
-						"id",null,
-						ContentSource.ATTRIBUTE, "id",
-						OntologyConstants.ONTOLOGY_NAMESPACE+"Set", 
-						OntologyConstants.ONTOLOGY_NAMESPACE+"identifierDramaQueen",
-						TargetPropertyType.DATATYPE_PROPERTY));
-		
-		
-		
-		
-		
-		
-		
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/sequences/Step", null, null,
-						null, null, OntologyConstants.ONTOLOGY_NAMESPACE+"SceneGroup", null, null));
-		
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/sequences/Step/children/Scene/children/Frame",
-						null, null,
-						null, null, OntologyConstants.ONTOLOGY_NAMESPACE+"Scene", null, null));
-
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/sequences/Step/children/Scene/children/Frame",
-						"id",null,
-						ContentSource.ATTRIBUTE, "id",
-						OntologyConstants.ONTOLOGY_NAMESPACE+"Scene", 
-						OntologyConstants.ONTOLOGY_NAMESPACE+"identifierDramaQueen",
-						TargetPropertyType.DATATYPE_PROPERTY));
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/sequences/Step/children/Scene/children/Frame",
-						null, null,
-						ContentSource.CONTAINMENT, "Step",
-						OntologyConstants.ONTOLOGY_NAMESPACE+"SceneGroup", 
-						OntologyConstants.ONTOLOGY_NAMESPACE+"hasScene",
-						TargetPropertyType.OBJECT_PROPERTY));
-		
-		
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/sequences/Step/children/Scene/children/Frame/Property",
-						"id", "168", ContentSource.ATTRIBUTE, "value",
-						OntologyConstants.ONTOLOGY_NAMESPACE+"Frame", 
-						OntologyConstants.ONTOLOGY_NAMESPACE+"interiorExterior",
-						TargetPropertyType.DATATYPE_PROPERTY));
-
-		mappings.add(
-				new MappingDefinition("/ScriptDocument/sequences/Step/children/Scene/children/Frame/Property",
-						"id", "10", ContentSource.REFERENCE, "value",
-						OntologyConstants.ONTOLOGY_NAMESPACE+"Frame", 
-						OntologyConstants.ONTOLOGY_NAMESPACE+"sceneSet",
-						TargetPropertyType.OBJECT_PROPERTY));
-		
-		*/
-
 		DramaQueenSource dqsoure = new DramaQueenSource();
 		InputStream inputStream = dqsoure.get(dqFile);
 		
@@ -147,10 +28,10 @@ public class DramaqueenToRdfTest {
 		
 		dqrdf.convert(inputStream);
 		
-		Model generatedModel = dqrdf.getGeneratedModel();
+		dqrdf.writeRdfToFile(outputFile);
 
-
-		generatedModel.write(System.out, "TTL");
+//		Model generatedModel = dqrdf.getGeneratedModel();
+//		generatedModel.write(System.out, "TTL");
 	}
 
 }
