8af92a50caf8167ab26511d72a806fb5c165ec82
fmeiners <fmeiners@uni-potsdam.de>
2015-09-16 16:47:17 +0200

    Enhanced preproducer xml exporter

--- de.dwerft.ln/src/main/java/de/dwerft/lpdc/exporter/PreproducerExporter.java
+++ de.dwerft.ln/src/main/java/de/dwerft/lpdc/exporter/PreproducerExporter.java
@@ -46,7 +46,7 @@ public class PreproducerExporter extends RdfExporter {
 		
 		//Prepare xml document template, including root, payload method, and project
 		Element root = new Element("root");
-		Element methodElement = new Element("payload").setAttribute(new Attribute("method", "postscript"));	
+		Element methodElement = new Element("payload").setAttribute(new Attribute("method", "postScript"));	
 		Element projectIdElement = new Element("project").setAttribute(new Attribute("projectid", projectID));
 		
 		//Spawn new document and append primary elements
@@ -64,14 +64,17 @@ public class PreproducerExporter extends RdfExporter {
 
 		//Get all episodes of the current project
 		ArrayList<Resource> episodes = getLinkedResources(project, "hasEpisode");
+		
 		//And the projects title
 		String projectTitle = getLinkedDataValues(project, "title").get(0).getString();
 		
 		//Iterating over each episode, append the episode element to the project element
-		for (Resource episode : episodes) {
+		for (Resource episode : episodes) {	
 			Element episodeElement = getEpisodeElement(episode, projectTitle);
 			projectIdElement.addContent(episodeElement);
 		}
+		
+		addNameSpaces(root);
 		
 		//Print the final xml file
 		writeXmlToFile(prpXml);
@@ -92,6 +95,11 @@ public class PreproducerExporter extends RdfExporter {
 		//Create basic elements
 		Element titleElement = new Element("title").setText(projectTitle);
 		Element episodeElement = new Element("episode").addContent(titleElement);
+		
+		//Add episode ID as attribute if there is one
+		List<Literal> episodeIDs = getLinkedDataValues(episode, "identifierPreProducer");
+		if (!episodeIDs.isEmpty())
+			episodeElement.setAttribute("id", episodeIDs.get(0).getString());
 		
 		//Get all scene groups of the episode
 		ArrayList<Resource> sceneGroups = getLinkedResources(episode, "hasSceneGroup");
@@ -126,33 +134,66 @@ public class PreproducerExporter extends RdfExporter {
 		Element sceneElement = new Element("scene");
 		Map<String, ArrayList<Literal>> allEpisodeDataValues = getAllLinkedDataValues(scene);
 		
-		//All elements are hard coded, might be better to find a genralized way of appending these
+		//All elements are hard coded, might be better to find a generalized way of appending these
 		//It is assumed the Arraylist only contains one value for each key
 		
-		ArrayList<Literal> literalList = null;
+		//scene decoration name | this is an attribute
+		ArrayList<Resource> sets = getLinkedResources(scene, "sceneSet");
+		if (sets.size() == 1) {
+			sets = getLinkedResources(sets.get(0), "setLocation");
+			
+			if (sets.size() == 1) {
+				String name = getLinkedDataValues(sets.get(0), "name").get(0).getString();
+				sceneElement.setAttribute("decorationname", name);
+			}
+		}
+			
+		
+		//scene inserted | this is an attribute
+		if (allEpisodeDataValues.containsKey("sceneInserted")) {
+			String insertedText = getSceneContentValue(allEpisodeDataValues, "sceneInserted");
+			String insertedBoolean = "true";
+			if (insertedText.equals("0"))
+				insertedBoolean = "false";
+			
+			sceneElement.setAttribute("insert", insertedBoolean);
+		}
+			
+		//scene number
+		if (allEpisodeDataValues.containsKey("sceneNumber"))
+			sceneElement.addContent(new Element("number").setText(getSceneContentValue(allEpisodeDataValues, "sceneNumber")));
 		
 		//interior/exterior
-		literalList = allEpisodeDataValues.get("interiorExterior");
-		if (literalList != null) {
-			String intExt = literalList.get(0).getString();
-			Element intExtElement = new Element("intext").setText(intExt);
-			sceneElement.addContent(intExtElement);
-		}
+		if (allEpisodeDataValues.containsKey("interiorExterior"))
+			sceneElement.addContent(new Element("intext").setText(getSceneContentValue(allEpisodeDataValues, "interiorExterior")));
 		
 		//day/night
-		literalList = allEpisodeDataValues.get("dayTime");
-		if (literalList != null) {
-			String dayNight = literalList.get(0).getString();
-			Element dayNightElement = new Element("daynight").setText(dayNight);
-			sceneElement.addContent(dayNightElement);
+		if (allEpisodeDataValues.containsKey("dayTime"))
+			sceneElement.addContent(new Element("daynight").setText(getSceneContentValue(allEpisodeDataValues, "dayTime")));
+		
+		//description
+		if (allEpisodeDataValues.containsKey("sceneDescription"))
+			sceneElement.addContent(new Element("description").setText(getSceneContentValue(allEpisodeDataValues, "sceneDescription")));
+		
+		//script | is nested in preproducer but not in rdf
+		if (allEpisodeDataValues.containsKey("sceneContent")) {
+			Element scriptElement = new Element("script").addContent(new Element("formattedscript").setText(getSceneContentValue(allEpisodeDataValues, "sceneContent")));
+			sceneElement.addContent(scriptElement);
 		}
-		
-		
-		//TODO add missing elements
-		//
-		
-		
+		//scene shots
+		if (allEpisodeDataValues.containsKey("sceneShots"))
+			sceneElement.addContent(new Element("shots").setText(getSceneContentValue(allEpisodeDataValues, "sceneShots")));
+			
+		/*
+		//story time
+		if (allEpisodeDataValues.containsKey("storyTime"))
+			sceneElement.addContent(getSceneContent(allEpisodeDataValues, "sceneShots", "shots"));
+		*/
 		return sceneElement;
+	}
+	
+	private String getSceneContentValue(Map<String, ArrayList<Literal>> allEpisodeDataValues, String ontologyIdentifier) {
+		return allEpisodeDataValues.get(ontologyIdentifier).get(0).getLexicalForm();
 	}
 
 	/**
@@ -163,12 +204,26 @@ public class PreproducerExporter extends RdfExporter {
 	private void addNameSpaces(Element root) {
 		
 	    if(root != null) {
-		    root.setNamespace(PRP_NAMESPACE);
+		    if (hasNamespace(root))
+	    		root.setNamespace(PRP_NAMESPACE);
+		    
 		    for (int index = 0; root.getChildren() != null &&  index < root.getChildren().size(); index++) {
 		        addNameSpaces((Element) root.getChildren().get(index));
 		    }
 	    }
 	}
+
+	/**
+	 * Returns true if the given element is supposed to have a namespace prefix and false otherwise
+	 * 
+	 * @param root
+	 * 			the element in question
+	 * @return
+	 */
+	private boolean hasNamespace(Element root) {
+		return !(root.getName().equals("title") /*|| !root.getName().equals("root")*/ || root.getName().equals("payload"));
+	}
+	
 	
 	/**
 	 * Writes the xml document in pretty print to console and a file
