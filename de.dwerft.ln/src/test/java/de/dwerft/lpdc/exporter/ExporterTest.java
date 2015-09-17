a8c6eb05999b897a48324d0d724f88633b251c97
hagt <henning.agt-rickauer@hpi.de>
2015-09-17 16:52:35 +0200

    Target project / queried project

--- de.dwerft.ln/src/test/java/de/dwerft/lpdc/exporter/ExporterTest.java
+++ de.dwerft.ln/src/test/java/de/dwerft/lpdc/exporter/ExporterTest.java
@@ -12,23 +12,30 @@ import de.dwerft.lpdc.sources.PreproducerSource;
 
 public class ExporterTest {
 
-	private final String tmpDir = System.getProperty("java.io.tmpdir");
+	private static final String PREPRODUCER_CONFIG_FILE = "src/main/resource/config.properties";
+
+//	private final String tmpDir = System.getProperty("java.io.tmpdir");
+	private final String tmpDir = "examples";
 	
-	@Test
-	public void testLockitExporter() throws IOException {
-		
-		LockitExporter e = new LockitExporter(OntologyConstants.SPARQL_ENDPOINT, OntologyConstants.ONTOLOGY_FILE, tmpDir + "/lockit_filmontology_scenes.csv", "17621");
-		e.export();
-	}
+//	@Test
+//	public void testLockitExporter() throws IOException {
+//		
+//		LockitExporter e = new LockitExporter(OntologyConstants.SPARQL_ENDPOINT, OntologyConstants.ONTOLOGY_FILE, tmpDir + "/lockit_filmontology_scenes.csv", "17621");
+//		e.export();
+//	}
 	
 	@Test
 	public void testPreproducerExporter() throws IOException {
 		
 		String outputPath = tmpDir + "/preproducer_filmontology_scenes.xml";
-		PreproducerExporter e = new PreproducerExporter(OntologyConstants.SPARQL_ENDPOINT, OntologyConstants.ONTOLOGY_FILE, outputPath, "17621");
+		PreproducerExporter e = new PreproducerExporter(
+				OntologyConstants.SPARQL_ENDPOINT,
+				OntologyConstants.ONTOLOGY_FILE,
+				outputPath, "9860f0bb-d9a6-45e4-9d03-79e7fefd16fa", "17621");
 		e.export();
 		
-		PreproducerSource prpSource = new PreproducerSource(new File("src/main/resource/config.preproducer"));
+		PreproducerSource prpSource = new PreproducerSource(new File(PREPRODUCER_CONFIG_FILE));
+		
 		prpSource.send(new String(Files.readAllBytes(Paths.get(outputPath))));
 	}
 }
