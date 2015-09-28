0a587a2484f14e5e38d3184ac854ca69088d55dd
fmeiners <fmeiners@uni-potsdam.de>
2015-09-28 13:54:50 +0200

    Added comments

--- de.dwerft.ln/src/main/java/de/dwerft/ln/SparqlExample.java
+++ de.dwerft.ln/src/main/java/de/dwerft/ln/SparqlExample.java
@@ -11,18 +11,33 @@ import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.ResultSet;
 import com.hp.hpl.jena.query.ResultSetFormatter;
 
-
+/**
+ * The Class SparqlExample.
+ * In order to access the information stored in the linked production data cloud, queries must be send to a SPARQL endpoint.
+ * This is an example demonstrating how to query the SPARQL endpoint. 
+ * The query used fetches the URIs of all projects currently stored in the LPDC.
+ */
 public class SparqlExample {
 
 	public static void main(String[] args) {
 		
+		//The URL of the SPARQL endpoint
 		String endpoint = "http://sparql.filmontology.org/";
-		String sparqlQuery = "select * from <http://filmontology.org/> where {<http://filmontology.org/resource/DWERFT> ?p ?o} ";
+		
+		//The query being used in this example fetches all projects currently stored in the lpdc
+		String sparqlQuery = "select * from <http://filmontology.org/> where {<http://filmontology.org/resource/DWERFT> ?predicate ?object} ";
  
+		//Sending queries requires authentication with a username and password
 		HttpAuthenticator authenticator = new SimpleAuthenticator("dwerft", "#dwerft".toCharArray());
-        Query query = QueryFactory.create(sparqlQuery); 
-        QueryExecution qExe = QueryExecutionFactory.sparqlService(endpoint, query , authenticator);
-        ResultSet results = qExe.execSelect();
+        
+		//Setting up the objects required by jena 
+		Query query = QueryFactory.create(sparqlQuery); 
+        QueryExecution queryExecution = QueryExecutionFactory.sparqlService(endpoint, query , authenticator);
+        
+        //Executes the prepared query. A query always returns a result set
+        ResultSet results = queryExecution.execSelect();
+        
+        //Print the result set to console
         ResultSetFormatter.out(System.out, results, query) ;
 	}
 
--- de.dwerft.ln/src/main/java/de/dwerft/lpdc/exporter/RdfExporter.java
+++ de.dwerft.ln/src/main/java/de/dwerft/lpdc/exporter/RdfExporter.java
@@ -26,8 +26,6 @@ import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QuerySolution;
 import com.hp.hpl.jena.query.ResultSet;
-import com.hp.hpl.jena.query.ResultSetFactory;
-import com.hp.hpl.jena.query.ResultSetFormatter;
 import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
@@ -38,17 +36,23 @@ import com.hp.hpl.jena.util.iterator.ExtendedIterator;
 
 import de.dwerft.lpdc.general.OntologyConstants;
 
+/**
+ * The Class RdfExporter.
+ * 
+ * Provides general helper methods for querying a SPARQL endpoint. 
+ */
 public abstract class RdfExporter {
-	
-	private final String FILMONTOLOGY_BASE_URI = "http://filmontology.org/ontology/1.0/";
-	
+		
 	/** The Logger. */
 	private static final Logger L = Logger.getLogger(RdfExporter.class.getName());
 
+	/** The model. */
 	private Model model;
 	
+	/** The sparql endpoint. */
 	private String sparqlEndpoint;
 	
+	/** The ontology model. */
 	private OntModel ontologyModel;
 	
 	/**
@@ -62,16 +66,20 @@ public abstract class RdfExporter {
 	public RdfExporter(String sparqlEndpointUrl, String ontologyFilename) {
 		L.info("Exporting RDF from SPARQL endpoint " + sparqlEndpointUrl);
 		this.sparqlEndpoint = sparqlEndpointUrl;
+		
+		//Initializing the underlying ontology model of the lpdc
 		this.ontologyModel = ModelFactory.createOntologyModel(OntModelSpec.OWL_MEM);
 		ontologyModel.read(ontologyFilename, OntologyConstants.ONTOLOGY_FORMAT);
 	}
 	
 	/**
-	 * This constructor is for use with an RDF file
-	 * 
-	 * @param rdfInput 
-	 * 				the file containing the data, in RDF format
-	 * @throws IOException
+	 * This constructor is for use with a local RDF file. 
+	 * It is encouraged to use a SPARQL endpoint instead of this local variant.
+	 *
+	 * @param rdfInput 				
+	 * 			the file containing the data, in RDF format
+	 * @throws IOException 
+	 * 			Signals that an I/O exception has occurred.
 	 */
 	public RdfExporter(File rdfInput) throws IOException {	
 		L.info("Exporting RDF from file " + rdfInput.getAbsolutePath());
@@ -79,19 +87,22 @@ public abstract class RdfExporter {
 	}
 	
 	/**
-	 * Prepares ARQ so regular SPARQL queries can be issued to the given file of RDF data
-	 * 
-	 * @throws IOException
+	 * Prepares ARQ so regular SPARQL queries can be issued to the given file of RDF data.
+	 *
+	 * @param rdfInput 
+	 * 			the file containing the data, in RDF format
+	 * @throws IOException 
+	 * 			Signals that an I/O exception has occurred.
 	 */
 	private void prepareARQ(File rdfInput) throws IOException {
 		L.info("Preparing ARQ for use with an RDF File");
 		
+		//Initializing the underlying basic model
 		InputStream in = new FileInputStream(rdfInput);
 		model = ModelFactory.createMemModelMaker().createModel("");
 		model.read(in, null, "TTL"); // null base URI, since model URIs are absolute
 		in.close();
 	}
-	
 	
 	/**
 	 * Gets the resources filtered by literal name and value.
@@ -102,6 +113,8 @@ public abstract class RdfExporter {
 	 * @return a list of resources
 	 */
 	public List<Resource> getResourcesFilteredByLiteral(String className, String datatypeName, String value) {
+		L.debug("Requesting resources with literal " + datatypeName + " and value " + value);
+		
 		List<Resource> result = new ArrayList<Resource>();
 		
 		for (Resource r : getResourcesByType(className)) {
@@ -111,13 +124,13 @@ public abstract class RdfExporter {
 				}
 			}
 		}
-		
 		return result;
 	}
 	
 	/**
 	 * Executes a query on the sparql endpoint and returns a result set.
-	 * 
+	 * @see  
+	 * 		SparqlExample
 	 * @param queryString
 	 * 						The sparql query string
 	 * @return	The query result set
@@ -134,11 +147,12 @@ public abstract class RdfExporter {
 	}
 	
 	/**
-	 * Retrieves a set of resources that have the specified type (ontology class name)
-	 * 
-	 * @param className
-	 * 					Name of the ontology class
-	 * @return All resources that have a RDF type relation to the ontology class
+	 * Retrieves a set of resources that have the specified type (ontology class name).
+	 *
+	 * @param className 					
+	 * 			Name of the ontology class
+	 * @return 
+	 * 			All resources that have a RDF type relation to the ontology class
 	 */
 	public ArrayList<Resource> getResourcesByType(String className) {	
 		L.debug("Requesting resources of type " + className);
@@ -162,7 +176,7 @@ public abstract class RdfExporter {
 	
 	/**
 	 * Retrieves a set of resources that are linked to the start resource
-	 * via the specified object property using default name space.
+	 * via the specified object property using default namespace.
 	 * 
 	 * @param start
 	 * 				Start resource (subject)
@@ -176,18 +190,18 @@ public abstract class RdfExporter {
 	
 	/**
 	 * Retrieves a set of resources that are linked to the start resource
-	 * via the specified object property. 
+	 * via the specified object property using a specified namespace.
 	 * 
 	 * @param start
 	 * 				Start resource (subject)
-	 * @param prefix
+	 * @param namespace
 	 * 				Namespace prefix of the object property
 	 * 
 	 * @param objectPropertyName
 	 * 				Name of the object property
 	 * @return Linked resources
 	 */
-	public ArrayList<Resource> getLinkedResources(Resource start, String prefix, String objectPropertyName) {
+	public ArrayList<Resource> getLinkedResources(Resource start, String namespace, String objectPropertyName) {
 		L.debug("Requesting resources linked to " + start.getLocalName() + " via object property " + objectPropertyName);
 		
 		ArrayList<Resource> result = new ArrayList<Resource>();
@@ -195,7 +209,7 @@ public abstract class RdfExporter {
 		String query = OntologyConstants.ONTOLOGY_PREFIXES 
 				+ "select ?res where { "
 				+ "<" + start.getURI() + "> "
-				+ prefix+":"+objectPropertyName + " "
+				+ namespace + ":" + objectPropertyName + " "
 				+ "?res"
 				+ "}";
 
@@ -212,26 +226,29 @@ public abstract class RdfExporter {
 	/**
 	 * Retrieves a set of data values (literals) that are linked to the start
 	 * resource via the specified datatype property. 
-	 * 
-	 * @param start
-	 * 				Start resource (subject)
-	 * @param datatypePropertyName
-	 * 				Name of the datatype property
-	 * @return
+	 *
+	 * @param start 				
+	 * 			Start resource (subject)
+	 * @param datatypePropertyName 				
+	 * 			Name of the datatype property
+	 * @return 
+	 * 			the linked data values
 	 */
 	public ArrayList<Literal> getLinkedDataValues(Resource start, String datatypePropertyName) {
 		L.debug("Requesting literals linked to " + start.getLocalName() + " via datatype property " + datatypePropertyName);
 		
+		//The resulting list
 		ArrayList<Literal> result = new ArrayList<Literal>();
 		
+		//The SPARQL query
 		String query = OntologyConstants.ONTOLOGY_PREFIXES 
 				+ "select ?res where { "
 				+ "<" + start.getURI() + "> "
-				+ OntologyConstants.ONTOLOGY_PREFIX+":"+datatypePropertyName + " "
+				+ OntologyConstants.ONTOLOGY_PREFIX + ":" + datatypePropertyName + " "
 				+ "?res"
 				+ "}";
 
-		
+		//Iterating over the result set, add only the resources in column "res"
 		ResultSet rs = queryEndpoint(query);
 		while(rs.hasNext()) {
 			QuerySolution sol = rs.nextSolution();
@@ -242,6 +259,16 @@ public abstract class RdfExporter {
 	}
 	
 	
+	/**
+	 * TODO
+	 * 
+	 * Gets the linked data values.
+	 *
+	 * @param start 
+	 * 			
+	 * @param datatypeProperties the datatype properties
+	 * @return the linked data values
+	 */
 	public Map<String,ArrayList<Literal>> getLinkedDataValues(Resource start, Set<Property> datatypeProperties) {
 		L.debug("Requesting literals linked to " + start.getLocalName() + " via datatype properties " + datatypeProperties);
 		
@@ -258,7 +285,6 @@ public abstract class RdfExporter {
 				+ "?prop "
 				+ "?lit"
 				+ "}";
-
 		
 		ResultSet rs = queryEndpoint(query);
 		while(rs.hasNext()) {
@@ -281,21 +307,18 @@ public abstract class RdfExporter {
 						result.put(propName, values);
 					}
 					values.add(literal);
-				}
-				
+				}			
 			}
 		}
 				
 		return result;
 	}
 	
-	
-	
 	/**
-	 * TODO
-	 * 
-	 * @param start
-	 * @return
+	 * TODO.
+	 *
+	 * @param start the start
+	 * @return the all linked data values
 	 */
 	public Map<String, ArrayList<Literal>> getAllLinkedDataValues(Resource start) {
 		
@@ -312,16 +335,7 @@ public abstract class RdfExporter {
 		Set<Property> declaredProperties = getDeclaredProperties(type);
 		
 		result = getLinkedDataValues(start, declaredProperties);
-		
-//		for (Property property : declaredProperties) {
-//			if (ontologyModel.getDatatypeProperty(property.getURI()) != null) {
-//				ArrayList<Literal> linkedDataValues = getLinkedDataValues(start, property.getLocalName());
-//				if (linkedDataValues.size() > 0) {
-//					result.put(property.getLocalName(), linkedDataValues);
-//				}
-//			}
-//		}
-//		
+
 		return result;
 				
 	}
@@ -329,9 +343,11 @@ public abstract class RdfExporter {
 	/**
 	 * Retrieves the set of declared properties in the ontology model
 	 * for the given ontology class.
+	 * 
 	 * @param ontologyClass
-	 * 						The ontology class to looked up
-	 * @return A set of property names
+	 * 			The ontology class to be looked up
+	 * @return 
+	 * 			A set of property names
 	 */
 	public Set<Property> getDeclaredProperties(Resource ontologyClass) {
 		L.debug("Requesting properties for ontology class " + ontologyClass.getLocalName());
@@ -347,77 +363,25 @@ public abstract class RdfExporter {
 					result.add(prop);
 				}
 			}
-		}
-		
+		}	
 		return result;
 	}
 	
 	/**
-	 * TODO convert to the new setup
-	 * Gets all scenes of a project
-	 * 
-	 * @param projectID
-	 * 				the ID  of the project containing the scenes.
-	 * 				TODO: !! Currently NOT used, all scenes are retrieved regardless of projectID !!
-	 * @return a XML String containing all scenes of an episode
-	 */
-	protected String getScenesAsXML(String projectID) {
-		
-		L.info("Requesting all scenes of project with ID " + projectID);;
-		
-		//Get all properties of the class scene
-		ResultSet rs = ResultSetFactory.fromXML(getAllClassPropertiesAsXML("Scene"));
-		
-		ArrayList<String> properties = new ArrayList<String>();
-		
-		//Append all properties to ArrayList
-		while (rs.hasNext()) {
-			QuerySolution qs = rs.next();
-			String variable = cropOntNameSpace(qs.get("property").toString());
-			
-			if (variable != null)
-				properties.add(variable);
-		}
-		
-		//Get a Stringbuilder for creating the query
-		StringBuilder sb = new StringBuilder();
-		
-		//Build SPARQL request
-		sb.append("PREFIX filmontology: <" + FILMONTOLOGY_BASE_URI + "> \nSELECT ?scene");
-		
-		for (String p : properties) {
-			
-			//properties with multiple values need to be grouped for distinct results and are separated by a blank
-			//for simplicity sake all properties are treated like they may contain multiple values
-			//therefore all variable names in the result set have a plural s
-			sb.append(" (CONCAT(GROUP_CONCAT(DISTINCT?" + p + ";SEPARATOR=' ')) as ?" + p + "s)");
-		}
-		
-		sb.append("\nWHERE {\n?scene a filmontology:Scene . \n");
-		
-		for (String p : properties)
-			sb.append("OPTIONAL {?scene filmontology:" + p + " ?" + p + " . }\n");
-		
-		sb.append("\n}\nGROUP BY ?scene");
-		
-		return executeQuery(sb.toString());
-	}
-	
-	/**
 	 * Extracts the value from a given query solution
-	 * Returns an empty string instead of null if the resource/literal is empty
-	 * 
-	 * @param q 
-	 * 			the QuerySolution
-	 * @param nodeName 
-	 * 			the name of the resource/literal
-	 * @return 
-	 * 			the value of the resource/literal or an empty string
+	 * Returns an empty string instead of null if the resource/literal is empty.
+	 *
+	 * @param querySolution 			
+	 * 		the QuerySolution
+	 * @param nodeName 			
+	 * 		the name of the resource/literal
+	 * @return 			
+	 * 		the value of the resource/literal or an empty string
 	 */
-	protected String getResourceOrLiteralValue(QuerySolution q, String nodeName) {
+	protected String getResourceOrLiteralValue(QuerySolution querySolution, String nodeName) {
 		
 		String result = "";
-		RDFNode node = q.get(nodeName);
+		RDFNode node = querySolution.get(nodeName);
 		
 		if (node != null) {
 			if (node.isResource()) {
@@ -432,62 +396,10 @@ public abstract class RdfExporter {
 		}
 		return result;
 	}
-	
+		
 	/**
-	 * Returns a XML String containing all properties of a class
-	 * 
-	 * @param OntClass, the name of the class
-	 * @return a ResultSet with the names of the properties
+	 * Abstract method to be overwritten by inheriting classes.
 	 */
-	private String getAllClassPropertiesAsXML(String OntClass) {
-		L.debug("Converting properties to XML format for class " + OntClass);
-		
-		String propertyQuery = "PREFIX filmontology: <" + FILMONTOLOGY_BASE_URI + "> "
-				+ "SELECT DISTINCT ?property "
-				+ "{?x a filmontology:" + OntClass + " . "
-				+ " ?x ?property ?value . }";
-		
-		return executeQuery(propertyQuery);
-	}
-	
-	/**
-	 * Executes a given SPARQL Query and returns the XML result
-	 * 
-	 * @param query
-	 * @return
-	 */
-	private String executeQuery(String query) {
-		L.debug("Executing SPARQL query " + query);
-		
-		Query q = QueryFactory.create(query);
-
-		//Execute the query and obtain results
-		QueryExecution qe = QueryExecutionFactory.create(q, model);
-		ResultSet results = qe.execSelect();
-		
-		//convert result to xml
-		String xmlOutput = ResultSetFormatter.asXMLString(results);
-		
-		//free up resources used running the query
-		qe.close();	
-
-		return xmlOutput;
-	}
-	
-	/**
-	 * Removes the base ontology URI from a given input String
-	 * If the String does not contain said URI, null is returned
-	 * 
-	 * @param input
-	 * @return
-	 */
-	private String cropOntNameSpace(String input) {
-		if (input.contains(FILMONTOLOGY_BASE_URI))
-			return input.replace(FILMONTOLOGY_BASE_URI, "");
-		
-		return null;
-	}
-		
 	public abstract void export();
 	
 }
