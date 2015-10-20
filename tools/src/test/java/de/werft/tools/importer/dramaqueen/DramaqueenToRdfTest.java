8363c12b8c21c37d44fbbf49ff2a8fd0dc2d5c6b
hagt <henning.agt-rickauer@hpi.de>
2015-10-20 13:18:16 +0200

    Directory typo fixed

--- tools/src/test/java/de/werft/tools/importer/dramaqueen/DramaqueenToRdfTest.java
+++ tools/src/test/java/de/werft/tools/importer/dramaqueen/DramaqueenToRdfTest.java
@@ -17,8 +17,8 @@ import de.werft.tools.general.OntologyConstants;
 public class DramaqueenToRdfTest {
 	
 	private static final String dqFile = "examples/Hansel_Gretel_de.dq";
-	private static final String outputFile = "examples/Hansel_Gretel_de_new.ttl";
-	private static final String DRAMAQUEEN_MAPPINGS_FILE = "src/main/resource/dramaqueen.mappings";
+	private static final String outputFile = "examples/Hansel_Gretel_de_new2.ttl";
+	private static final String DRAMAQUEEN_MAPPINGS_FILE = "src/main/resources/dramaqueen.mappings";
 
 	@Test
 	public void testConverter() {
