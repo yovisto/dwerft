1c511e4372658e217208da2288f1543b07f829bc
Henrik Jürges <juerges.henrik@gmail.com>
2016-07-01 14:10:36 +0200

    removed exec:java; added primitve test

--- service/src/test/java/de/werft/MyResourceTest.java
+++ service/src/test/java/de/werft/MyResourceTest.java
@@ -1,40 +1,17 @@
 package de.werft;
 
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.junit.After;
-import org.junit.Before;
+import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Application;
 
 import static org.junit.Assert.assertEquals;
 
-public class MyResourceTest {
+public class MyResourceTest extends JerseyTest {
 
-    private HttpServer server;
-    private WebTarget target;
-
-    @Before
-    public void setUp() throws Exception {
-        // start the server
-        server = Main.startServer();
-        // create the client
-        Client c = ClientBuilder.newClient();
-
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        //c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
-
-        target = c.target(Main.BASE_URI);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        server.stop();
+    @Override
+    protected Application configure() {
+        return new UploadApp();
     }
 
     /**
@@ -42,7 +19,7 @@ public class MyResourceTest {
      */
     @Test
     public void testGetIt() {
-        String responseMsg = target.path("myresource").request().get(String.class);
-        assertEquals("Got it!", responseMsg);
+        String response = target("upload").request().get(String.class);
+        assertEquals("Got it!", response);
     }
 }
