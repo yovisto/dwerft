b54cb7289c6a100ac5c643a53672d055d4090241
hagt <henning.agt-rickauer@hpi.de>
2016-11-03 10:03:36 +0100

    Missing super constructor

--- tools/src/main/java/de/werft/tools/rmllib/preprocessing/DramaqueenPreprocessor.java
+++ tools/src/main/java/de/werft/tools/rmllib/preprocessing/DramaqueenPreprocessor.java
@@ -1,19 +1,5 @@
 package de.werft.tools.rmllib.preprocessing;
 
-import de.werft.tools.general.Document;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -25,6 +11,22 @@ import java.util.HashMap;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import de.werft.tools.general.Document;
+
 /**
  * This preprocessor takes a dramaqueen file and unzips the xml
  * file. In a second step the xml file is adjusted to match
@@ -35,7 +37,7 @@ import java.util.zip.ZipFile;
 public class DramaqueenPreprocessor extends BasicPreprocessor {
 
     public DramaqueenPreprocessor(String projectUri) {
-
+    	super(projectUri);
     }
 
     @Override
